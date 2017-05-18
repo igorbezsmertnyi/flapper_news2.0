1ff47f5e950ee65ade62a44b51a62c0e118b3746
Igor <mr.igor96@outlook.com>
2017-05-18 20:19:23 +0200

    fixed registration link

--- client/src/app/components/LoginForm/login.component.ts
+++ client/src/app/components/LoginForm/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core'
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
-import { Router } from '@angular/router'
 import { AuthService } from '../../auth.service'
 import { validateEmail } from '../../validators/validators'
 
@@ -15,22 +14,15 @@ export class LogIn {
 
   hasError: boolean = false
   user: any = {}
-  currentLocation: string
 
   @Output() UserData = new EventEmitter()
 
   constructor(protected authService: AuthService,
-              private router: Router,
               private fb: FormBuilder) {
     this.logInForm = this.fb.group({
       email: new FormControl('', [Validators.required, validateEmail]),
       password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
     })
-  }
-
-  ngOnInit() {
-    this.currentLocation = this.router.routerState.snapshot.url
-    window.location.href = `${this.currentLocation}#login`
   }
 
   logIn(f) {
